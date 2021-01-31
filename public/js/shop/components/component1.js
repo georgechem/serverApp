84aae6deda72f72c24e682f46b514e77e9ab5738
georgechem <54766527+georgechem@users.noreply.github.com>
2021-01-31 22:36:31 +0000

    GSAP

--- public/js/shop/components/component1.js
+++ public/js/shop/components/component1.js
@@ -1,3 +1,19 @@
 let mainMenuButton = document.createElement('div');
 mainMenuButton.innerHTML = `<i id="mainMenuButton" class="fas fa-bars"></i>`;
 
+let mainMenu = document.createElement('div');
+mainMenu.classList.add('menu');
+mainMenu.id = 'menu';
+
+let mainMenuList = document.createElement('ul');
+mainMenuList.classList.add('menu__list');
+
+let mainMenuItem = document.createElement('li');
+mainMenuItem.classList.add('menu__list__item');
+
+let mainMenuLink = document.createElement('a');
+mainMenuLink.classList.add('menu__list__item__link');
+mainMenuLink.classList.add('primary');
+mainMenuLink.innerHTML = `HOME`;
+
+mainMenu.appendChild(mainMenuList.appendChild(mainMenuItem.appendChild(mainMenuLink)));
--- public/js/shop/index/javascript1.js
+++ public/js/shop/index/javascript1.js
@@ -1,2 +1,13 @@
 let mainMenuButtonHook = document.getElementById('nav');
 mainMenuButtonHook.appendChild(mainMenuButton);
+
+mainMenuButtonHook.appendChild(mainMenu);
+
+mainMenuButton.addEventListener('click', function(){
+    TweenMax.to('#menu', 1, {left: 0, backgroundColor: '#111111'});
+    mainMenuButton.classList.toggle('hidden');
+})
+
+
+
+
