10be11d51a128bb9a9bcfb9ae149b3aa736e7207
georgechem <54766527+georgechem@users.noreply.github.com>
2021-02-01 09:42:25 +0000

    html conntent moved to twig instead working with it in javascript

--- public/js/shop/index/javascript1.js
+++ public/js/shop/index/javascript1.js
@@ -1,13 +1,15 @@
-let mainMenuButtonHook = document.getElementById('nav');
-mainMenuButtonHook.appendChild(mainMenuButton);
 
-mainMenuButtonHook.appendChild(mainMenu);
+let mainMenuButton = document.getElementById('mainMenuButton');
 
 mainMenuButton.addEventListener('click', function(){
     TweenMax.to('#menu', 1, {left: 0, backgroundColor: '#111111'});
     mainMenuButton.classList.toggle('hidden');
 })
 
-
+let mainMenuExitButtonHook = document.getElementById('exitMenuButton');
+mainMenuExitButtonHook.addEventListener('click', function(){
+    TweenMax.to('#menu', 1, {left: '-100%', backgroundColor: '#fff' });
+    mainMenuButton.classList.toggle('hidden');
+});
 
 
